77275e21da1cb689e423a3562f839b0d44903dfd
Danil <pingvin5332@gmail.com>
2022-12-02 18:35:10 +0300

    Commit

--- src/main/java/com/example/javafx3/Controller7.java
+++ src/main/java/com/example/javafx3/Controller7.java
@@ -1,4 +1,11 @@
 package com.example.javafx3;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 public class Controller7 {
 }
--- src/main/java/com/example/javafx3/Window6.java
+++ src/main/java/com/example/javafx3/Window6.java
@@ -1,4 +1,23 @@
 package com.example.javafx3;
 
-public class Window6 {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Window6 extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Window6.class.getResource("Window6.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 400);
+        stage.setTitle("Выберите место");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
